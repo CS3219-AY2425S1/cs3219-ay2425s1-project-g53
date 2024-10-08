0e2eb06f004dd056c7a8487f21f9c0276b3b08ef
ginloy <liyongleow@gmail.com>
2024-10-09 00:26:34 +0800

    Add more detail to error messages

--- peerprep/src/lib/utils.ts
+++ peerprep/src/lib/utils.ts
@@ -2,11 +2,19 @@ import { Result, ResultAsync } from "neverthrow"
 import { z } from "zod"
 
 export const fetchResult = (url: RequestInfo | URL, init?: RequestInit) => {
-  return ResultAsync.fromThrowable(fetch, () => "Failed to fetch from " + url.toString())(url, init);
+  return ResultAsync.fromThrowable(fetch, (e) =>
+    `Failed to fetch from ${url}:
+  ${e}`
+  )(url, init);
 }
 
 export const zodParseResult = <T extends any, S extends z.ZodTypeAny>(data: T, schema: S): Result<z.infer<typeof schema>, string> => {
-  return Result.fromThrowable(schema.parse, () => "Failed to validate type\n" + `EXPECTED: ${typeof schema}\n` + `RECEIVED: ${typeof data}`)(data);
+  return Result.fromThrowable(schema.parse, (e) =>
+    `Failed to validate type 
+  EXPECTED: ${typeof schema}
+  RECEIVED: ${typeof data}
+  ${e}`)
+    (data);
 }
 
 export const parseJsonResult = (str: string) => {
@@ -14,5 +22,7 @@ export const parseJsonResult = (str: string) => {
 }
 
 export const validateResponse = <S extends z.ZodTypeAny>(response: Response, schema: S): ResultAsync<z.infer<typeof schema>, string> => {
-  return ResultAsync.fromPromise(response.json(), () => "Response is not json").andThen(o => zodParseResult(o, schema))
+  return ResultAsync.fromPromise(response.json(), (e) =>
+    `Response is not json
+  ${e}`).andThen(o => zodParseResult(o, schema))
 }
