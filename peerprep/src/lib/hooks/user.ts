0d5a7bb74516eb66a442ec749228d1e2cf20dfd8
ginloy <liyongleow@gmail.com>
2024-10-09 22:50:38 +0800

    Delete uncessary hooks

--- peerprep/src/lib/hooks/user.ts
+++ peerprep/src/lib/hooks/user.ts
@@ -1,37 +1,5 @@
 import { useEffect, useState } from "react";
-import { login, currentUser, UserWithToken, UserCreate, createUser } from '@/actions/user'
-import { pipeResult } from "../utils";
-import { redirectAction } from "@/actions/utils";
-
-export const useLogin = (redirect?: string) => {
-  const [error, setError] = useState<string | null>(null);
-  const loginHook = async (email: string, password: string) => {
-    await pipeResult(login, email, password).then(e => e.match(
-      async _ => {
-        if (redirect) await redirectAction(redirect);
-      },
-      msg => {
-        setError(msg)
-      }
-    ))
-  }
-  return { login: loginHook, loginError: error }
-}
-
-export const useSignup = (redirect?: string) => {
-  const [error, setError] = useState<string | null>(null);
-  async function signup(data: UserCreate) {
-    await pipeResult(createUser, data).then(r => r.match(
-      async _ => {
-        if (redirect) await redirectAction(redirect);
-      },
-      e => {
-        setError(e);
-      }
-    ))
-  }
-  return { signup: signup, signupError: error }
-}
+import { currentUser, UserWithToken } from '@/actions/user'
 
 export const useCurrentUser = () => {
   const [user, setUser] = useState<UserWithToken | null>(null);
