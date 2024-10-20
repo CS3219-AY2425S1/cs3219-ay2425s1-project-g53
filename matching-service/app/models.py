570570f5cad31b580fa6d0ac7830563d07081fb2
CaspianMike-L <looweesing@gmail.com>
2024-10-20 21:03:24 +0800

    fix: change data type mispell

--- matching-service/app/models.py
+++ matching-service/app/models.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class UserRequest(BaseModel):
-    user_id: string
+    user_id: str
     question_id: int
 
 class Match(BaseModel):
-    user_1: string
-    user_2: string
+    user_1: str
+    user_2: str
     question_id: int
     match_time: datetime
