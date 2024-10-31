ba03ac3daa22da3a7a3be9e40b034056c4c437de
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-10-31 15:18:56 +0800

    Add server config and attempt model

---
+++ history-service/models/attemptModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const AttemptSchema = mongoose.schema({
+    users: [
+        { type: String, required: true }
+    ],
+    problem: { type: String, required: true },
+    attemptStart: { type: Date },
+    attemptEnd: { type: Date },
+    attemptCode : { type: Buffer, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Attempt', AttemptSchema);
