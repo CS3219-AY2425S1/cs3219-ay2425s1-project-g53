2f4f50bd8cdb77171017bbe3b8d18932c6b63adc
ginloy <liyongleow@gmail.com>
2024-10-31 20:13:16 +0800

    Collab service v1

---
+++ collab-service/README.md
@@ -0,0 +1,44 @@
+# Collab service for peerprep
+
+## Environment variables
+### PORT (default: 3000)
+Port the server will listen on.
+
+### TIMEOUT (default: 60)
+Time in seconds the server will wait before deleting persisted data.
+
+## Endpoints
+### /ws/:session (WEBSOCKET)
+Websocket endpoint for yjs/hocuspocus providers.
+Use session name as yjs document.
+#### Request params
+session: Session Name returned by /create
+
+### /create (POST)
+Attempt to create a session with 2 users and a question.
+
+#### Request Body
+
+```json
+  {
+    user_1: string,
+    user_2: string,
+    question_id: number  
+  }
+```
+
+#### Response
+##### 200
+Returns session name in body. Existing session name is returned if session already exists.
+##### 400
+Returned if request body fails to parse.
+
+### /sessions/:userid (GET)
+#### Request Params
+userid: unique identifier of user
+
+#### Response Body
+
+```json
+[session1, sessions2, ...] | []
+```
--- collab-service/src/index.ts
+++ collab-service/src/index.ts
@@ -1,18 +1,58 @@
-import  express from 'express';
+import express from 'express';
 import expressWebsockets from 'express-ws';
 import 'dotenv/config';
-import { Server } from '@hocuspocus/server';
+import { Document, Server } from '@hocuspocus/server';
+import { MatchSchema, SessionManager } from "./session_manager"
 
 const { app } = expressWebsockets(express());
 const PORT = process.env.PORT ?? '3000';
+const TIMEOUT = (() => {
+	const temp = process.env.TIMEOUT;
+	if (!temp) {
+		return 60;
+	}
+	try {
+		return parseInt(temp);
+	} catch (error) {
+		console.log("Failed to parse ${tmep} to number, using default timeout value");
+		return 60;
+	}
+})();
+
+const sessionManager = new SessionManager();
 
 const server = Server.configure({
+	async onStoreDocument(data) {
+		sessionManager.saveSession(data.documentName, data.document);
+	},
+	async onLoadDocument(data): Promise<Document> {
+		return sessionManager.getSession(data.documentName)?.document ?? new Document(data.documentName);
+	},
 })
 
-app.ws("/", (websocket, request) => {
-	console.log(request);
+app.ws("/ws/:session", (websocket, request) => {
+	const sessionName = request.params.session;
+	if (!sessionManager.getSession(sessionName)) {
+		return;
+	}
 	server.handleConnection(websocket, request);
 })
+
+app.post("/create", (request, response) => {
+	try {
+		const match = MatchSchema.parse(request.body);
+		const sessionName = sessionManager.createSession(match);
+		response.status(200).send(sessionName);
+	} catch (error) {
+		response.status(400).send("Failed to parse request body");
+	}
+});
+
+app.get("/sessions/:userid", (request, response) => {
+	const user = request.params.userid;
+	const sessions = sessionManager.getSessions(user);
+	response.status(200).send(sessions);
+});
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`)
