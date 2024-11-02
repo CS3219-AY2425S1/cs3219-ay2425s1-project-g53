653b5469b8716eb36e6536987543c43b0bb3ef11
JonathanChongLyenChyn <jonchong98@gmail.com>
2024-11-02 17:46:15 +0800

    Add button to download code attempt in attempt history

--- peerprep/src/components/attempt-table.tsx
+++ peerprep/src/components/attempt-table.tsx
@@ -5,9 +5,28 @@ import { Anchor, Table, Text } from "@mantine/core";
 import Link from "next/link";
 import { UserContext } from "@/lib/contexts";
 import { use } from "react";
+import AttemptDownload from "./attempt-download";
+import { IconDownload } from "@tabler/icons-react";
 
 export default function AttemptTable(props: { attempts: Attempt[] }) {
   const user = use(UserContext);
+
+  const handleDownload = async (attemptCode: string) => {
+    try {
+      const blob = new Blob([attemptCode], { type: 'text/plain' });
+      const url = window.URL.createObjectURL(blob);
+
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'AttemptCode.txt'; // Specify the file name
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+      window.URL.revokeObjectURL(url);
+    } catch (err) {
+      console.error("Error downloading attempt code:", err);
+    }
+  };
 
   const rows = props.attempts.map(q => (
     <Table.Tr>
@@ -17,7 +36,13 @@ export default function AttemptTable(props: { attempts: Attempt[] }) {
       <Table.Td>{q.problem}</Table.Td>
       <Table.Td>{q.attemptStart}</Table.Td>
       <Table.Td>{q.attemptEnd}</Table.Td>
-      <Table.Td>{q.attemptCode}</Table.Td>
+      <Table.Td>
+        <span
+          onClick={() => handleDownload(q.attemptCode)}
+          style={{ cursor: 'pointer', color: 'blue', textDecoration: 'underline' }}
+        ><IconDownload></IconDownload>
+        </span>
+      </Table.Td>
     </Table.Tr>));
 
   const headers = (
