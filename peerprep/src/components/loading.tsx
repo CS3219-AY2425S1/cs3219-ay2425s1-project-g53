d6f76cf07d210cf848978313a742ab851f4cf4d9
ginloy <liyongleow@gmail.com>
2024-11-02 21:04:25 +0800

    Add loading component

--- peerprep/src/components/loading.tsx
+++ peerprep/src/components/loading.tsx
@@ -2,7 +2,7 @@ import { Center, Loader } from "@mantine/core";
 
 export default function Loading({h, size}: {h?: string | number, size?: number}) {
   return (
-    <Center w="100%" h={h}>
+    <Center w="100%" h={h ?? "100%"}>
       <Loader size={size}/>
     </Center>
   )
