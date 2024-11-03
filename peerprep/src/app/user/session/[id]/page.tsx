f9d7fc2c58a37457e8532d8bc8a981612a6d191e
ginloy <liyongleow@gmail.com>
2024-11-04 01:04:46 +0800

    Dont want to set NEXT_PUBLIC inside Dockerfile

--- peerprep/src/app/user/session/[id]/page.tsx
+++ peerprep/src/app/user/session/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { getUserSessions } from '@/actions/collab';
+import { getCollabWsUrl } from '@/actions/url';
 import { currentUser } from '@/actions/user';
 import CodeEditor from '@/components/editor';
 import { Container } from '@mantine/core'
 import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
+  const url = await getCollabWsUrl();
   const user = await currentUser();
 
   const userSessions = await getUserSessions(user!);
@@ -16,7 +18,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <Container h="calc(100vh - 60px)" fluid>
-      <CodeEditor sessionName={sessionName} user={user!} />
+      <CodeEditor sessionName={sessionName} user={user!} wsUrl={url} />
     </Container>
   )
 }
