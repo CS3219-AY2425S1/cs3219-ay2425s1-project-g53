9f6e22c8f365051b10f4961ae90f4293e4c878b7
ginloy <liyongleow@gmail.com>
2024-11-04 13:15:10 +0800

    add question display and panels to session page

--- peerprep/src/app/questions/[id]/page.tsx
+++ peerprep/src/app/questions/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getQuestion, Question } from "@/actions/questions";
+import QuestionDisplay from "@/components/question";
 import { Text, Divider, Stack, Title, Badge, Group, ScrollArea, Flex, Box } from "@mantine/core";
 import Markdown from 'react-markdown';
 import remarkGfm from "remark-gfm";
@@ -9,37 +10,7 @@ export default async function Page({ params }: { params: { id: number } }) {
 
   const question: Question = await getQuestion(params.id);
 
-  const difficultyBadgeColor = (() => {
-    switch (question.complexity) {
-      case "Easy":
-        return "green";
-      case "Medium":
-        return "yellow";
-      case "Hard":
-        return "red";
-    }
-  })()
-
-  const markdown = (
-    <Markdown remarkPlugins={[remarkGfm]}>{`# ${question.title}\n${question.description}`}</Markdown>
-  )
-
   return (
-    <Stack py="md" px="md" h="calc(100vh - 60px)">
-      <Group wrap="nowrap">
-        <Group flex="0 0 auto" h="100%">
-          <Title size="xl">{question.title}</Title>
-          <Badge color={difficultyBadgeColor}>{question.complexity}</Badge>
-          <Divider size="sm" orientation="vertical" />
-        </Group>
-        <Group>
-          {question.categories.map(c => <Badge key={c.id}>{c.name}</Badge>)}
-        </Group>
-      </Group>
-      <Divider />
-      <ScrollArea >
-        {markdown}
-      </ScrollArea>
-    </Stack>
+    <QuestionDisplay  py={20} h="calc(100vh - 60px)" px={20} question={question} />
   )
 }
